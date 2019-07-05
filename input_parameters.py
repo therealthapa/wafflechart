51caafe050b603d10109b5c0cf81520e79c4ca72
surenbthapa <47115320+surenbthapa@users.noreply.github.com>
2019-07-05 13:19:22 +0530

    Create input_parameters.py

---
+++ input_parameters.py
@@ -0,0 +1,7 @@
+width = 40 # width of chart
+height = 10 # height of chart
+
+categories = df_dsn.index.values # categories
+values = df_dsn['Total'] # correponding values of categories
+
+colormap = plt.cm.coolwarm # color map class
